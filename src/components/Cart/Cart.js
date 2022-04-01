a899778f5c50a7983baa04e4f1b3c9708e722b55
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-04-01 22:31:35 +0600

    removed item successfully

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import './Cart.css';
 const Cart = ({cart,handleRemoveFromCart}) => {
-    console.log(cart)
+    // console.log(cart[0]._id)
+     // Conditional rendering options
+    // 1. Element variable
+    // 2. ternary operator condition ? true : false   
+    // 3. && Operator (shorthand)
+    // 4. || 
+    let command ;
+    if(cart.length === 0){
+        command = <div>
+             <h5>ohh Kipta, khoroch kor</h5>
+            <p>kinos na ken</p>
+        </div>
+    }
     return (
         <div>
             <h3>Items Selected: {cart.length}</h3>
@@ -11,6 +23,16 @@ const Cart = ({cart,handleRemoveFromCart}) => {
                     <button onClick={() => handleRemoveFromCart(tShirt)}>X</button>
                     </p>)
             }
+                {cart.length === 0 || <p className='orange'>YAY! You are buying</p>}
+            {/* jodi length 0 na hoy tahole element a jabe */}
+            {cart.length === 3 && <div className='orange'> 
+                <h3>Trigonal</h3>
+                <p>Tin jon ke ki gift diba</p>
+            </div>}
+            {command}
+            {cart.length !==4 ? <p>Keep adding</p> : <button>Remove ALL</button>}
+            {cart.length ===4 && <button className='orange'>Review Order</button>}
+                {/* jodi length 4 hoy tahole element a jabe */}
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,37 +1,49 @@
-import React, { useState } from 'react';
-import useTshirts from '../../hooks/useTshirts';
-import Cart from '../Cart/Cart';
-import Tshirt from '../Tshirt/Tshirt';
-import './Home.css';
+import React, { useState } from "react";
+import useTshirts from "../../hooks/useTshirts";
+import Cart from "../Cart/Cart";
+import Tshirt from "../Tshirt/Tshirt";
+import "./Home.css";
 const Home = () => {
-    const [tShirts] = useTshirts();
-    const [cart, setCart] = useState([]); //obj gulu k niye array er moddhe rakhtece
-    console.log(tShirts)
-    const handleAddToCart = (selectedItems) =>{
-        // console.log(selectedItems)
-        const newCart = [...cart, selectedItems];
-        setCart(newCart);
+  const [tShirts] = useTshirts();
+  const [cart, setCart] = useState([]); //obj gulu k niye array er moddhe rakhtece
+  console.log(tShirts);
+  const handleAddToCart = (selectedItems) => {
+    const exist = cart.find((tShirt) => tShirt._id === selectedItems._id);
+    if (!exist) {
+      const newCart = [...cart, selectedItems];
+      setCart(newCart);
     }
+    else{
+        alert('items already added');
+    }
+  };
 
-    const handleRemoveFromCart =(selectedItems) =>{
-        console.log(selectedItems)
-        
-    }
-    return (
-        <div>
-             <div className="home-container">
-                 <div className="tshirt-container">
-                     {
-                        tShirts?.map(tShirt => <Tshirt key={tShirt._id} tShirt={tShirt} handleAddToCart={handleAddToCart}   ></Tshirt>)
-                        
-                     }
-                 </div>
-                 <div className="cart-container">
-                    <Cart cart = {cart} handleRemoveFromCart={handleRemoveFromCart}> </Cart>
-                </div>
-             </div>
+  const handleRemoveFromCart = (selectedItems) => {
+    console.log(selectedItems);
+    const rest = cart.filter((tShirt) => tShirt._id !== selectedItems._id);
+    setCart(rest);
+  };
+  return (
+    <div>
+      <div className="home-container">
+        <div className="tshirt-container">
+          {tShirts?.map((tShirt) => (
+            <Tshirt
+              key={tShirt._id}
+              tShirt={tShirt}
+              handleAddToCart={handleAddToCart}
+            ></Tshirt>
+          ))}
         </div>
-    );
+        <div className="cart-container">
+          {/* <Cart  key={cart[0]?._id}  cart = {cart} handleRemoveFromCart={handleRemoveFromCart}> </Cart> */}
+          <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart}>
+            {" "}
+          </Cart>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
